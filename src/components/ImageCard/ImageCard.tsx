8953c0ba019aaf5b78d563b52f460905590513d2
Andrii Semenenko <oxonomy14@gmail.com>
2025-05-23 23:51:59 +0700

    fix2

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -24,7 +24,7 @@ const ImageCard: FC<ImageCardProps> = ({ item, onClick }) => {
         <p>
           <span>Author: </span>
           <a
-            href={item.links.portfolio}
+            href={item.links.portfolio ?? "#"}
             target="_blank"
             rel="noopener noreferrer"
           >
